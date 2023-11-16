729f917fb9570b10cda5395a4120d7b852062a7f
Daniel Kastl <daniel@georepublic.de>
2023-11-16 10:25:24 +0900

    Fixes issue on other pages
    Signed-off-by: Daniel Kastl <daniel@georepublic.de>

--- src/index.ts
+++ src/index.ts
@@ -2,60 +2,63 @@ import { Template, BLANK_PDF } from '@pdfme/common';
 import { Designer } from '@pdfme/ui';
 
 document.addEventListener("DOMContentLoaded", function() {
-  const designerContainer = document.getElementById('pdfme-designer') as HTMLElement;
-  const basepdfField = document.getElementById('print_template_basepdf') as HTMLInputElement;
-  const schemasField = document.getElementById('print_template_schemas') as HTMLInputElement;
-
-  let designer: Designer | undefined;
-  let currentSchemas = schemasField.value ? JSON.parse(schemasField.value) : [];
-
+  const designerContainer = document.getElementById('pdfme-designer');
+  const basepdfField = document.getElementById('print_template_basepdf') as HTMLInputElement | null;
+  const schemasField = document.getElementById('print_template_schemas') as HTMLInputElement | null;
   const openBtn = document.getElementById('open-designer-fullscreen-btn');
   const closeBtn = document.getElementById('close-designer-fullscreen-btn');
   const designerFullscreen = document.getElementById('designer-fullscreen');
   const uploadField = document.getElementById('pdf-upload') as HTMLInputElement | null;
   const useBlankPdfLink = document.getElementById('use-blank-pdf');
 
-  openBtn?.addEventListener('click', function() {
-    const basePdfValue = basepdfField.value ? basepdfField.value : BLANK_PDF;
+  if (designerContainer && openBtn && closeBtn && designerFullscreen && basepdfField && schemasField) {
+    let designer: Designer | undefined;
+    let currentSchemas = schemasField.value ? JSON.parse(schemasField.value) : [];
 
-    const template: Template = {
-      basePdf: basePdfValue,
-      schemas: currentSchemas
-    };
+    openBtn?.addEventListener('click', function() {
+      const basePdfValue = basepdfField.value ? basepdfField.value : BLANK_PDF;
 
-    if (!designer && designerContainer) {
-      designer = new Designer({
-        domContainer: designerContainer,
-        template: template
-      });
-      designer.onChangeTemplate((updatedTemplate) => {
-        currentSchemas = updatedTemplate.schemas;
-        schemasField.value = JSON.stringify(currentSchemas);
-      });
-    } else {
-      designer?.updateTemplate(template);
+      const template: Template = {
+        basePdf: basePdfValue,
+        schemas: currentSchemas
+      };
+
+      if (!designer && designerContainer) {
+        designer = new Designer({
+          domContainer: designerContainer,
+          template: template
+        });
+        designer.onChangeTemplate((updatedTemplate) => {
+          currentSchemas = updatedTemplate.schemas;
+          schemasField.value = JSON.stringify(currentSchemas);
+        });
+      } else {
+        designer?.updateTemplate(template);
+      }
+
+      designerFullscreen.style.display = 'block';
+    });
+
+    closeBtn?.addEventListener('click', function() {
+      designerFullscreen.style.display = 'none';
+      schemasField.value = JSON.stringify(currentSchemas);
+    });
+  }
+
+  if (uploadField && useBlankPdfLink && basepdfField) {
+    useBlankPdfLink?.addEventListener('click', function(event) {
+      event.preventDefault();
+      basepdfField.value = '';
+      if (uploadField) uploadField.value = '';
+      toggleBasePDFControls();
+    });
+
+    uploadField?.addEventListener('change', function() {
+      if (this instanceof HTMLInputElement) {
+        encodeBasePDF(this);
+      }
+    });
     }
-
-    designerFullscreen.style.display = 'block';
-  });
-
-  closeBtn?.addEventListener('click', function() {
-    designerFullscreen.style.display = 'none';
-    schemasField.value = JSON.stringify(currentSchemas);
-  });
-
-  useBlankPdfLink?.addEventListener('click', function(event) {
-    event.preventDefault();
-    basepdfField.value = '';
-    if (uploadField) uploadField.value = '';
-    toggleBasePDFControls();
-  });
-
-  uploadField?.addEventListener('change', function() {
-    if (this instanceof HTMLInputElement) {
-      encodeBasePDF(this);
-    }
-  });
 });
 
 function encodeBasePDF(input: HTMLInputElement) {
