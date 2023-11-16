692c88ce62581123a75c6728d9db8e0cc6c6bf23
Daniel Kastl <daniel@georepublic.de>
2023-11-16 10:14:11 +0900

    Supports schema persistence
    Signed-off-by: Daniel Kastl <daniel@georepublic.de>

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,88 @@
 import { Template, BLANK_PDF } from '@pdfme/common';
 import { Designer } from '@pdfme/ui';
 
-const designerContainer = document.getElementById('pdfme-designer');
-let designer: Designer | undefined;
-
-document.getElementById('open-designer-fullscreen-btn').addEventListener('click', function() {
+document.addEventListener("DOMContentLoaded", function() {
+  const designerContainer = document.getElementById('pdfme-designer') as HTMLElement;
   const basepdfField = document.getElementById('print_template_basepdf') as HTMLInputElement;
-  const basePdfValue = basepdfField.value ? basepdfField.value : BLANK_PDF;
+  const schemasField = document.getElementById('print_template_schemas') as HTMLInputElement;
 
-  const template: Template = {
-    basePdf: basePdfValue,
-    schemas: [
-      {
-        a: {
-          type: 'text',
-          position: { x: 0, y: 0 },
-          width: 10,
-          height: 10,
-        },
-        b: {
-          type: 'text',
-          position: { x: 10, y: 10 },
-          width: 10,
-          height: 10,
-        },
-        c: {
-          type: 'text',
-          position: { x: 20, y: 20 },
-          width: 10,
-          height: 10,
-        },
-      },
-    ],
-  };
+  let designer: Designer | undefined;
+  let currentSchemas = schemasField.value ? JSON.parse(schemasField.value) : [];
 
-  if (designerContainer) {
-    if (!designer) {
+  const openBtn = document.getElementById('open-designer-fullscreen-btn');
+  const closeBtn = document.getElementById('close-designer-fullscreen-btn');
+  const designerFullscreen = document.getElementById('designer-fullscreen');
+  const uploadField = document.getElementById('pdf-upload') as HTMLInputElement | null;
+  const useBlankPdfLink = document.getElementById('use-blank-pdf');
+
+  openBtn?.addEventListener('click', function() {
+    const basePdfValue = basepdfField.value ? basepdfField.value : BLANK_PDF;
+
+    const template: Template = {
+      basePdf: basePdfValue,
+      schemas: currentSchemas
+    };
+
+    if (!designer && designerContainer) {
       designer = new Designer({
         domContainer: designerContainer,
-        template: template,
+        template: template
+      });
+      designer.onChangeTemplate((updatedTemplate) => {
+        currentSchemas = updatedTemplate.schemas;
+        schemasField.value = JSON.stringify(currentSchemas);
       });
     } else {
-      designer.updateTemplate(template);
+      designer?.updateTemplate(template);
     }
-  }
 
-  // Display the designer fullscreen
-  document.getElementById('designer-fullscreen').style.display = 'block';
+    designerFullscreen.style.display = 'block';
+  });
+
+  closeBtn?.addEventListener('click', function() {
+    designerFullscreen.style.display = 'none';
+    schemasField.value = JSON.stringify(currentSchemas);
+  });
+
+  useBlankPdfLink?.addEventListener('click', function(event) {
+    event.preventDefault();
+    basepdfField.value = '';
+    if (uploadField) uploadField.value = '';
+    toggleBasePDFControls();
+  });
+
+  uploadField?.addEventListener('change', function() {
+    if (this instanceof HTMLInputElement) {
+      encodeBasePDF(this);
+    }
+  });
 });
+
+function encodeBasePDF(input: HTMLInputElement) {
+  if (input.files && input.files[0]) {
+    const reader = new FileReader();
+    reader.onload = function(e) {
+      const encodedPDF = e.target.result as string;
+      const basepdfField = document.getElementById('print_template_basepdf') as HTMLInputElement | null;
+      if (basepdfField) {
+        basepdfField.value = encodedPDF;
+      }
+      toggleBasePDFControls();
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+function toggleBasePDFControls() {
+  const basepdfField = document.getElementById('print_template_basepdf') as HTMLInputElement | null;
+  const basepdfIcon = document.getElementById('basepdf-ok-icon');
+  const useBlankPdfLink = document.getElementById('use-blank-pdf');
+
+  if (basepdfField && basepdfField.value) {
+    basepdfIcon.style.display = 'inline';
+    useBlankPdfLink.style.display = 'inline';
+  } else {
+    basepdfIcon.style.display = 'none';
+    useBlankPdfLink.style.display = 'none';
+  }
+}
